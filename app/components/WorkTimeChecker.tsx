4016be949f963d768d1bdab5943f64f7907229af
TKMhub <111867865+TKMhub@users.noreply.github.com>
2023-10-18 23:14:11 +0900

    complete

--- app/components/WorkTimeChecker.tsx
+++ app/components/WorkTimeChecker.tsx
@@ -1,10 +1,14 @@
 "use client";
 import React, { useState } from "react";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 import {
   Container,
   TextField,
   FormControl,
-  InputLabel,
   Select,
   MenuItem,
   Button,
@@ -12,6 +16,22 @@ import {
 } from "@mui/material";
 
 const WorkTimeChecker: React.FC = () => {
+  const [employeeId, setEmployeeId] = useState("");
+  const [month, setMonth] = useState("");
+  const [workTime, setWorkTime] = useState("");
+  const [holidayTime, setHolidayTime] = useState("");
+  const [open, setOpen] = useState(false);
+
+  // ダイアログを開く
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  // ダイアログを閉じる
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <Container>
       <Grid container spacing={3} direction="column" alignItems="center">
@@ -27,13 +47,14 @@ const WorkTimeChecker: React.FC = () => {
             label="社員番号:"
             variant="outlined"
             style={{ width: "200px" }}
+            onChange={(e) => setEmployeeId(e.target.value)}
           />
         </Grid>
 
         <Grid item>
           <p className="text-left text-xs text-gray-500">対象月：</p>
           <FormControl variant="outlined" style={{ width: "200px" }}>
-            <Select>
+            <Select value={month} onChange={(e) => setMonth(e.target.value)}>
               <MenuItem value={1}>1月</MenuItem>
               <MenuItem value={2}>2月</MenuItem>
               <MenuItem value={3}>3月</MenuItem>
@@ -45,7 +66,7 @@ const WorkTimeChecker: React.FC = () => {
               <MenuItem value={9}>9月</MenuItem>
               <MenuItem value={10}>10月</MenuItem>
               <MenuItem value={11}>11月</MenuItem>
-              <MenuItem value={6}>6月</MenuItem>
+              <MenuItem value={12}>12月</MenuItem>
             </Select>
           </FormControl>
         </Grid>
@@ -55,6 +76,7 @@ const WorkTimeChecker: React.FC = () => {
             label="合計勤務時間:"
             variant="outlined"
             style={{ width: "200px" }}
+            onChange={(e) => setWorkTime(e.target.value)}
           />
         </Grid>
 
@@ -63,6 +85,7 @@ const WorkTimeChecker: React.FC = () => {
             label="法定休日時間:"
             variant="outlined"
             style={{ width: "200px" }}
+            onChange={(e) => setHolidayTime(e.target.value)}
           />
         </Grid>
 
@@ -70,12 +93,41 @@ const WorkTimeChecker: React.FC = () => {
           <Button
             variant="contained"
             color="primary"
-            style={{ width: "200px" }}
+            style={{ marginTop: "20px", width: "200px" }}
+            onClick={handleClickOpen}
           >
             登録
           </Button>
         </Grid>
       </Grid>
+      {/* ダイアログの定義 */}
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"登録情報"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {`社員番号：${employeeId}`}
+            <br />
+            {`対象月：${month}月`}
+            <br />
+            {`合計勤務時間：${workTime}`}
+            <br />
+            {`法定休日時間：${holidayTime}`}
+            <br />
+            <br />
+            {"上記の内容で登録しました。"}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            閉じる
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Container>
   );
 };
