39a06772951cc3d33a9ee0d99f479896ab7d1fc3
TKMhub <111867865+TKMhub@users.noreply.github.com>
2023-10-18 22:39:46 +0900

    init

--- app/components/WorkTimeChecker.tsx
+++ app/components/WorkTimeChecker.tsx
@@ -1,52 +1,69 @@
-import React from 'react';
-import { Container, TextField, FormControl, InputLabel, Select, MenuItem, Button } from '@mui/material';
+"use client";
+import React, { useState } from "react";
+import {
+  Container,
+  TextField,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Button,
+  Grid,
+} from "@mui/material";
 
-// WorkTimeChecker コンポーネントの定義
 const WorkTimeChecker: React.FC = () => {
   return (
     <Container>
-      {/* タイトル */}
-      <h1>法定超過時間チェッカー</h1>
-      
-      {/* 社員番号 */}
-      <div>
-        <label>■社員番号:</label>
-        <TextField variant="outlined" />
-      </div>
+      <Grid container spacing={3} direction="column" alignItems="center">
+        <Grid item sx={{ padding: "20px" }}>
+          <h1 className="p-2 text-center font-sans text-gray-600 font-extrabold text-2xl ">
+            TimeChecker
+          </h1>
+          <h2 className="p-1 text-center text-xm">法定超過時間チェッカー</h2>
+        </Grid>
 
-      {/* 対象月 */}
-      <div>
-        <FormControl variant="outlined">
-          <InputLabel>■対象月：</InputLabel>
-          <Select>
-            <MenuItem value={1}>1月</MenuItem>
-            <MenuItem value={2}>2月</MenuItem>
-            {/* 他の月も追加できます */}
-          </Select>
-        </FormControl>
-      </div>
+        <Grid item>
+          <TextField label="社員番号:" variant="outlined" />
+        </Grid>
 
-      {/* 勤務時間入力 */}
-      <h2>■勤務時間入力：</h2>
+        <Grid item>
+          <p className="text-left text-xs text-gray-500">対象月：</p>
+          <FormControl variant="outlined" style={{ width: "200px" }}>
+            <Select>
+              <MenuItem value={1}>1月</MenuItem>
+              <MenuItem value={2}>2月</MenuItem>
+              <MenuItem value={3}>3月</MenuItem>
+              <MenuItem value={4}>4月</MenuItem>
+              <MenuItem value={5}>5月</MenuItem>
+              <MenuItem value={6}>6月</MenuItem>
+              <MenuItem value={7}>7月</MenuItem>
+              <MenuItem value={8}>8月</MenuItem>
+              <MenuItem value={9}>9月</MenuItem>
+              <MenuItem value={10}>10月</MenuItem>
+              <MenuItem value={11}>11月</MenuItem>
+              <MenuItem value={6}>6月</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
 
-      <div>
-        {/* 合計勤務時間 */}
-        <div>
-          <label>合計勤務時間:</label>
-          <TextField variant="outlined" />
-        </div>
+        <Grid item>
+          <TextField label="合計勤務時間:" variant="outlined" />
+        </Grid>
 
-        {/* 法定休日時間 */}
-        <div>
-          <label>法定休日時間（日曜勤務）:</label>
-          <TextField variant="outlined" />
-        </div>
-      </div>
+        <Grid item>
+          <TextField label="法定休日時間:" variant="outlined" />
+        </Grid>
 
-      {/* チェックボタン */}
-      <Button variant="contained" color="primary">
-        チェック
-      </Button>
+        <Grid item>
+          <Button
+            variant="contained"
+            color="primary"
+            style={{ width: "200px" }}
+          >
+            登録
+          </Button>
+        </Grid>
+      </Grid>
     </Container>
   );
 };
